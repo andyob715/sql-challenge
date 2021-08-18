f2d4cfffd022ddf534c8f3db3ccdf6ab4aa3f4f9
Andy O'Bryan <andrew.obryan@gmail.com>
2021-08-17 22:31:41 -0400

    finished SQL queries, pandas work still TBD

--- queries_sql.sql
+++ queries_sql.sql
@@ -37,9 +37,30 @@ departments.dept_no = dept_emp.dept_no;
 -- This returns more rows than employees, is this expected
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select first_name,last_name,sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON
+departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales' ;
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON
+departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development' ;
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count(last_name) as Frequency
+FROM employees
+GROUP BY last_name
+ORDER BY Frequency DESC;
